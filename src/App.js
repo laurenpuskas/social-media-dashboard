d7daeb1d3c104d5df8a5c8f5dfff5ae0d1e01a35
Liliripe <liliripe.studio@gmail.com>
2021-10-24 15:28:48 -0500

    init dark/light mode switch function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  const body = document.body
+  const lightTheme = 'light'
+  const darkTheme = 'dark'
+
+  // assign local property: theme
+  let theme
+
+  // check if localStorage already contains theme
+  if (localStorage) {
+    theme = localStorage.getItem('theme')
+  }
+
+  // if theme has been selected, apply it
+  // otherwise, default to light theme
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme)
+  } else {
+    body.classList.add(lightTheme)
+  }
+
+  // handle theme switch
+  const switchTheme = () => {
+    if (theme === darkTheme) {
+      body.classList.replace(darkTheme, lightTheme)
+      localStorage.setItem('theme', 'light')
+      theme = lightTheme
+    } else {
+      body.classList.replace(lightTheme, darkTheme)
+      localStorage.setItem('theme', 'dark')
+      theme = darkTheme
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={switchTheme}>change theme</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
